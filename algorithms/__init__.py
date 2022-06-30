1e1a505d0d8fb03577fe42ea0e777a0ac18bc1ac
g0bel1n <lucas.saban@icloud.com>
2022-06-30 16:14:45 +0200

    cleannup

--- algorithms/__init__.py
+++
@@ -1,6 +0,0 @@
-from .AS.as_algo import AS_Algo
-from .ACS_ranked.acs_algo import ACS_Algo
-from .double_ACS_ranked.double_acs_algo import double_ACS_ranked_algo
-
-
-__all__ = [AS_Algo, ACS_Algo, double_ACS_ranked_algo]
