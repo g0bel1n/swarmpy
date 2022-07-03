22bd2b996deaeab66a72680fd82c19bef065acdf
g0bel1n <lucas.saban@icloud.com>
2022-07-03 18:48:42 +0200

    advanced use notebook

--- swarmpy_tsp/aco_pipeline.py
+++ swarmpy_tsp/aco_pipeline.py
@@ -10,13 +10,20 @@ class ACO_Pipeline(ACO_Step):
         self,
         steps: list[tuple[str, ACO_Step]],
         verbose=0,
+        iter_max = 20,
+        as_step=False, 
+        last_step = False, 
+        metapipeline = False
     ):
         self.steps = steps
         self.verbose = verbose
         self.G: dict[str, np.ndarray]
         self.solutions = []
         self.ant_params: Optional[dict] = None
-        self.iter_max: int
+        self.iter_max: int = iter_max
+        self.as_step = False if  last_step else as_step
+        self.last_step = last_step
+        self.metapipeline = metapipeline
 
     def iter(self, run_params: dict[str, np.ndarray]):
         """
@@ -38,7 +45,7 @@ class ACO_Pipeline(ACO_Step):
                 run_params[k] = v
         return run_params["solutions"]
 
-    def run(self, G, iter_max=20):
+    def run(self, G):
         """
         > The function `run` takes as input a graph `G` and a maximum number of iterations `iter_max`
         and returns a list of solutions
@@ -48,7 +55,6 @@ class ACO_Pipeline(ACO_Step):
         :return: The best solutions found by the algorithm.
         """
         self.G = G
-        self.iter_max = iter_max
         run_params = {
             "ant_params": self.ant_params,
             "G": self.G,
@@ -58,10 +64,20 @@ class ACO_Pipeline(ACO_Step):
 
         solutions_bank = []
         pbar = tqdm(range(self.iter_max), desc="SwarmPy", ascii="░▒█")
-        for i in pbar :
+        for i in pbar:
             run_params["nb_iter"] = i
             solutions = self.iter(run_params=run_params)
             solutions_bank.append(solutions[0])
-            pbar.set_description(f'SwarmPy | Best score : {solutions[0][1]}')
+            pbar.set_description(f'SwarmPy |{"Step|" if self.as_step   else "Final Step|" if self.last_step else ""} Score : {solutions[-1][1] if self.metapipeline else solutions[0][1]}')
 
-        return solutions_bank
+
+        if self.as_step : 
+            return {"G": self.G}
+
+        elif self.last_step : 
+            return {'solutions' : solutions_bank}
+
+        elif self.metapipeline:
+            return {'solutions' : run_params['solutions'] }
+
+        return {'solutions' : solutions_bank}
